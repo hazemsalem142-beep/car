2e5dd218312220b23d28f8e3accc7ca20b604a2b
hazemsalem142-beep <hazemsalem142@gmail.com>
2025-08-13 23:03:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,412 @@
+// let face = document.getElementById('face')
+// let door = document.getElementById('door')
+// let container = document.getElementById('container')
+// let borders = document.getElementById('borders')
+// let will = document.getElementById('will')
+// let wont = document.getElementById('wont')
+// let door2 = document.getElementById('door2')
+// let glasses = document.getElementById('glasses')
+// let light = document.getElementById('light')
+// let light2 = document.getElementById('light2')
+// let point = document.getElementById('point')
+// let i1 = document.getElementById('i1')
+// let i2 = document.getElementById('i2')
+// let i3 = document.getElementById('i3')
+// let i4 = document.getElementById('i4')
+// let i5 = document.getElementById('i5')
+
+
+
+
+
+
+let face = document.getElementById('face')
+let door = document.getElementById('door')
+let container = document.getElementById('container')
+let borders = document.getElementById('border')
+let will = document.getElementById('will')
+let wont = document.getElementById('wont')
+let door2 = document.getElementById('door2')
+let glasses = document.getElementById('glasses')
+let light = document.getElementById('light')
+let light2 = document.getElementById('light2')
+let point = document.getElementById('point')
+let i1 = document.getElementById('i1')
+let i2 = document.getElementById('i2')
+let i3 = document.getElementById('i3')
+let i4 = document.getElementById('i4')
+let i5 = document.getElementById('i5')
+let delever = document.getElementById('delever')
+let images = document.getElementById('images')
+let parent = document.getElementById('parent')
+
+
+
+document.onkeyup = (e) => {
+    if (e.key === 'c'){
+        location.replace('hazemsalem142-beep.github.io/box/')
+    }
+}
+
+onload  = () => {
+    setTimeout(() => {
+
+        alert("if you want to change your categorys press the letter 'c' after pressing ok ('this for computers or laps only), if you don't click on the car to delever.")
+    },100)
+}
+
+borders.onclick = () => {
+        light.style.transition = '1.5s'
+        light2.style.transition = '1.5s'
+        light.style.background = 'gold'
+        light2.style.background = 'gold'
+        light2.style.boxShadow = '0 0 20px gold'
+        light.style.boxShadow = '0 0 20px gold'
+        setTimeout(() => {
+parent.style.transition = '10s'
+ parent.style.marginLeft = '9000px'    
+setTimeout(() => {
+swal.fire({
+    imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQioeLmqP2lcXUc1iefS2w2duxCFH1tBlwt79xt-IlMMUc2wWqryOxJ-kU&s',
+    title:'the order is delevering',
+    text:'your order is delevering'
+})
+
+setTimeout(() => {
+    swal.fire({
+        icon:'success',
+        title:'the order has been delevred.',
+        text:'your order has been delevred.'
+    })
+    setTimeout(() => {
+        location.replace('hazemsalem142-beep.github.io/box/')
+    },3000)
+},3000)
+},3000)
+        },1000)
+        
+    // 
+}
+
+glasses.onclick = () => {
+    light.style.transition = '1.5s'
+        light2.style.transition = '1.5s'
+        light.style.background = 'gold'
+        light2.style.background = 'gold'
+        light2.style.boxShadow = '0 0 20px gold'
+        light.style.boxShadow = '0 0 20px gold'
+        setTimeout(() => {
+parent.style.transition = '10s'
+ parent.style.marginLeft = '9000px'    
+setTimeout(() => {
+swal.fire({
+    imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQioeLmqP2lcXUc1iefS2w2duxCFH1tBlwt79xt-IlMMUc2wWqryOxJ-kU&s',
+    title:'the order is delevering',
+    text:'your order is delevering'
+})
+
+setTimeout(() => {
+    swal.fire({
+        icon:'success',
+        title:'the order has been delevred.',
+        text:'your order has been delevred.'
+    })
+    settimeout(() => {
+        location.replace('hazemsalem142-beep.github.io/box/')
+    },3000)
+},3000)
+},3000)
+        },1000)
+        
+}
+
+face.onclick = () => {
+    light.style.transition = '1.5s'
+        light2.style.transition = '1.5s'
+        light.style.background = 'gold'
+        light2.style.background = 'gold'
+        light2.style.boxShadow = '0 0 20px gold'
+        light.style.boxShadow = '0 0 20px gold'
+        setTimeout(() => {
+parent.style.transition = '10s'
+ parent.style.marginLeft = '9000px'    
+setTimeout(() => {
+swal.fire({
+    imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQioeLmqP2lcXUc1iefS2w2duxCFH1tBlwt79xt-IlMMUc2wWqryOxJ-kU&s',
+    title:'the order is delevering',
+    text:'your order is delevering'
+})
+
+setTimeout(() => {
+    swal.fire({
+        icon:'success',
+        title:'the order has been delevred.',
+        text:'your order has been delevred.'
+    })
+    setTimeout(() => {
+        location.replace('hazemsalem142-beep.github.io/box/')
+    },3000)
+},3000)
+},3000)
+        },1000)
+        
+}
+
+light.onclick = () => {
+    light.style.transition = '1.5s'
+        light2.style.transition = '1.5s'
+        light.style.background = 'gold'
+        light2.style.background = 'gold'
+        light2.style.boxShadow = '0 0 20px gold'
+        light.style.boxShadow = '0 0 20px gold'
+        setTimeout(() => {
+parent.style.transition = '10s'
+ parent.style.marginLeft = '9000px'    
+setTimeout(() => {
+swal.fire({
+    imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQioeLmqP2lcXUc1iefS2w2duxCFH1tBlwt79xt-IlMMUc2wWqryOxJ-kU&s',
+    title:'the order is delevering',
+    text:'your order is delevering'
+})
+
+setTimeout(() => {
+    swal.fire({
+        icon:'success',
+        title:'the order has been delevred.',
+        text:'your order has been delevred.'
+    })
+    settimeout(() => {
+        location.replace('hazemsalem142-beep.github.io/box/')
+    },3000)
+},3000)
+},3000)
+        },1000)
+        
+}
+
+light2.onclick = () => {
+    light.style.transition = '1.5s'
+        light2.style.transition = '1.5s'
+        light.style.background = 'gold'
+        light2.style.background = 'gold'
+        light2.style.boxShadow = '0 0 20px gold'
+        light.style.boxShadow = '0 0 20px gold'
+        setTimeout(() => {
+parent.style.transition = '10s'
+ parent.style.marginLeft = '9000px'    
+setTimeout(() => {
+swal.fire({
+    imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQioeLmqP2lcXUc1iefS2w2duxCFH1tBlwt79xt-IlMMUc2wWqryOxJ-kU&s',
+    title:'the order is delevering',
+    text:'your order is delevering'
+})
+
+setTimeout(() => {
+    swal.fire({
+        icon:'success',
+        title:'the order has been delevred.',
+        text:'your order has been delevred.'
+    })
+    settimeout(() => {
+        location.replace('hazemsalem142-beep.github.io/box/')
+    },3000)
+},3000)
+},3000)
+        },1000)
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function i1fun(){
+//     point.style.display = 'block'
+//     point.style.top = '20px'
+//     point.style.left = '270px'
+//     container.style.left = '300px'
+//     setTimeout(() => {
+//     point.style.left = '313px'
+
+//         point.style.top = '455px'
+//         delever.style.display = 'block'
+//     },1000)
+// }
+
+
+// i1.onclick = () => {
+
+// }
+
+// i2.onclick = () => {
+//     point.style.display = 'block'
+//     point.style.top = '20px'
+//     point.style.left = '460px'
+//     container.style.left = '300px'
+//     setTimeout(() => {
+//         point.style.left = '313px'
+    
+//             point.style.top = '455px'
+//         delever.style.display = 'block'
+
+//         },1000)
+// }
+
+// i3.onclick = () => {
+//     point.style.display = 'block'
+//     point.style.top = '20px'
+//     point.style.left = '650px'
+//     container.style.left = '300px'
+//     setTimeout(() => {
+//         point.style.left = '313px'
+    
+//             point.style.top = '455px'
+//         delever.style.display = 'block'
+
+//         },1000)
+// }
+
+// i4.onclick = () => {
+//     point.style.display = 'block'
+//     point.style.top = '20px'
+//     point.style.left = '840px'
+//     container.style.left = '300px'
+//     setTimeout(() => {
+//         point.style.left = '313px'
+    
+//             point.style.top = '455px'
+//         delever.style.display = 'block'
+
+//         },1000)
+// }
+
+// i5.onclick = () => {
+//     point.style.display = 'block'
+//     point.style.top = '20px'
+//     point.style.left = '1030px'
+//     container.style.left = '300px'
+//     setTimeout(() => {
+//         point.style.left = '313px'
+    
+//             point.style.top = '455px'
+//         delever.style.display = 'block'
+
+//         },1000)
+// }
+
+// delever.onclick = () => {
+//     door.style.top = '423px'
+//     door2.style.top = '477px'
+//  setTimeout(() => {
+//              container.style.left = '589px'
+//              point.style.left = '601px'
+// },1000)
+// setTimeout(() => {
+//  door.style.top = '446px'
+//  door2.style.top = '463px'
+//  setTimeout(() => {
+//      delever.style.display = 'none'
+// light.style.background = 'gold'
+// light2.style.background = 'gold'
+// i1.onclick = () => {
+//     console.log('you cant')
+// }
+// i2.onclick = () => {
+//     console.log('you cant')
+// }
+// i3.onclick = () => {
+//     console.log('you cant')
+// }
+// i4.onclick = () => {
+//     console.log('you cant')
+// }
+// i5.onclick = () => {
+//     console.log('you cant')
+// }
+
+// // setTimeout(() => {
+// // face.style.left = '-300px'
+// // light.style.left = '-300px'
+// // light2.style.left = '-300px'
+// // glasses.style.left = '-300px'
+// // container.style.left = '-300px'
+// // point.style.left = '-300px'
+// // borders.style.left = '1000px'
+// // door.style.left = '1000px'
+// // door2.style.left = '1000px'
+// // },1000)
+// face.style.left = '621px'
+// light.style.left = '653px'
+// light2.style.left = '653px'
+// glasses.style.left = '626px'
+// borders.style.left = '583px'
+// door.style.left = '583px'
+// door2.style.left = '583px'
+// setTimeout(() => {
+
+//     face.style.left = (parseInt(face.style.left) + 1000) + 'px';
+//     light.style.left = (parseInt(light.style.left) + 1000) + 'px';
+//     light2.style.left = (parseInt(light2.style.left) + 1000) + 'px';
+//     glasses.style.left = (parseInt(glasses.style.left) + 1000) + 'px';
+//     container.style.left = (parseInt(container.style.left) + 1000) + 'px';
+//     point.style.left = (parseInt(point.style.left) + 1000) + 'px';
+//     borders.style.left = (parseInt(borders.style.left) + 1000) + 'px';
+//     door.style.left = (parseInt(door.style.left) + 1000) + 'px';
+//     door2.style.left = (parseInt(door2.style.left) + 1000) + 'px';
+//     setTimeout(() => {
+// parent.style.display = 'none'
+// light2.style.left = '-653px'
+// glasses.style.left = '-626px'
+// borders.style.left = '-583px'
+// door.style.left = '-583px'
+// door2.style.left = '-583px'
+// parent.style.display = 'none'
+// will.style.display = 'block'
+// will.style.left = '500px'
+// setTimeout(() => {
+// will.style.left = '2000px'
+// setTimeout(() => {
+// will.style.display = 'none'
+// wont.style.display = 'block'
+// wont.style.left = '500px'
+// setTimeout(() => {
+// wont.innerHTML += '👍'
+// setTimeout(() => {
+//     wont.style.left = '2000px'
+// light2.style.left = '-653px'
+// glasses.style.left = '-626px'
+// borders.style.left = '-583px'
+// door.style.left = '-583px'
+// door2.style.left = '-583px'
+// setTimeout(() => {
+
+// location.reload()
+// },1000)
+// },2000)
+// },2000)
+// },2000)
+// },2000)
+//     },1000)
+// }, 1000)
+
+//  },2000)
+//  },2000)
+// }
+
+
